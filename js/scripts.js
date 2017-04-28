cac17ea732b52d59f3bc76de95b4caddef561431
Joe Parsley <voodoojoenut@gmail.com>
2017-04-28 11:53:35 -0700

    add fully working if and else if statements for the track selector

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,38 @@
 // BACK-END LOGIC
-var add = function(question1, question2, question3, question4, question5) {
-  return question1 + question2 + question3 + question4 + question5;
+var add = function(questionInput1, questionInput2, questionInput3, questionInput4, questionInput5) {
+  return questionInput1 + questionInput2 + questionInput3 + questionInput4 + questionInput5;
 }
 
-
-
-
+// var totalInput = function(add) {
+//   return
+// }
 
 
 
 // FRONT-END LOGIC
 $(document).ready(function() {
   $("#questions").submit(function(event) {
-    event.preventDefault();
-    var questionInput1 = parseInt($("input#question1").val());
-    var questionInput2 = parseInt($("input#question2").val());
-    var questionInput3 = parseInt($("input#question3").val());
-    var questionInput4 = parseInt($("input#question4").val());
-    var questionInput5 = parseInt($("input#question5").val());
+    // GATHERING INFORMATION VARS
+    var questionInput1 = parseInt($("#question1").val());
+    var questionInput2 = parseInt($("#question2").val());
+    var questionInput3 = parseInt($("#question3").val());
+    var questionInput4 = parseInt($("#question4").val());
+    var questionInput5 = parseInt($("#question5").val());
+    var totalInput = add(questionInput1, questionInput2, questionInput3, questionInput4, questionInput5);
+    $(".init-hidden").hide();
+    // TRACK-MATCH
+    if ( totalInput <= 5 ) {
+      $("#ruby-track").show();
+    } else if ( totalInput > 5 && totalInput <= 10) {
+      $("#php-track").show();
+    } else if ( totalInput > 10 && totalInput <= 15) {
+      $("#java-track").show();
+    } else if ( totalInput > 15 && totalInput <= 20) {
+      $("#css-track").show();
+    } else if ( totalInput > 20 && totalInput <= 25) {
+      $("#csharp-track").show();
+    }
 
+    event.preventDefault();
   });
 });
